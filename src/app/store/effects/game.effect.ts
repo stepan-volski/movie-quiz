40418d2952030aff5a42807a72829a1980111bc4
alexd1991 <a.dobrovolsky1991@gmail.com>
2022-06-22 11:32:23 +0300

    preloader

--- src/app/store/effects/game.effect.ts
+++ src/app/store/effects/game.effect.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { from, map, of, switchAll, switchMap, tap } from 'rxjs';
 import { FilmShortResponse } from 'src/app/models/filmShortResponse.model';
 import { RequestsService } from 'src/app/requests.service';
 import { gameInit, gameStarted, updateMovies } from '../actions/game.actions';
-import { IMovie } from '../state/app.state';
+import { IAppState, IMovie } from '../state/app.state';
 
 @Injectable()
 export class GameEffects {
@@ -14,21 +15,15 @@ export class GameEffects {
       switchMap(() =>
         this._requestsService.getTopMovies().pipe(
           map((movies: IMovie[]) => updateMovies({ movies })),
-          tap(() => {
-            console.log('game started');
-            return gameStarted();
-          })
+          tap(() => this._store.dispatch(gameStarted()))
         )
       )
-      // tap((movies) => console.log(movies)),
-      // map(() => gameStarted())
     )
   );
 
-  // checkGameStarted$ = createEffect(() => this._actions$.pipe());
-
   constructor(
     private _actions$: Actions,
-    private _requestsService: RequestsService
+    private _requestsService: RequestsService,
+    private _store: Store<IAppState>
   ) {}
 }
