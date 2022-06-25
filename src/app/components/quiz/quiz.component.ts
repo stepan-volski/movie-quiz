f4aedcaf605f2c8f4ebc0311d775dbe738951cf4
alexd1991 <a.dobrovolsky1991@gmail.com>
2022-06-25 23:15:40 +0300

    style timer

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -3,9 +3,13 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { GameStatus } from 'src/app/models/game-status';
+import { MovieLoadingStatus } from 'src/app/models/movie-loading-status';
 import { QuestionStatus } from 'src/app/models/question-status';
 import { TIMER } from 'src/app/shared/constants';
-import { skipQuestion, startLoadingCurrentMovie } from 'src/app/store/actions/game.actions';
+import {
+  skipQuestion,
+  startLoadingCurrentMovie,
+} from 'src/app/store/actions/game.actions';
 import {
   getGameData,
   getGameScore,
@@ -28,11 +32,11 @@ export class QuizComponent implements OnInit, OnDestroy {
   questionsLeftStr: string = '';
   remainingTime = TIMER;
   timerInterval?: ReturnType<typeof setInterval>;
+  isMovieLoaded: boolean = false;
 
   constructor(private _store: Store<IAppState>) {}
 
   ngOnInit(): void {
-
     this.subscription = this.game$.subscribe((gameData) => {
       this.status = gameData.status;
       this.score = gameData.score;
@@ -42,23 +46,32 @@ export class QuizComponent implements OnInit, OnDestroy {
           (film) => film.status === QuestionStatus.NotAnswered
         ).length
       } / ${gameData.allMoviesInGame.length}`;
+      this.isMovieLoaded =
+        gameData.allMoviesInGame[gameData.currentMovieIndex]?.loadingStatus ===
+          MovieLoadingStatus.Loaded || false;
     });
-    // this._store.dispatch(startLoadingCurrentMovie());
 
-    this.timerInterval = setInterval(() => {
-      if (this.remainingTime !== 0) {
-        this.remainingTime -= 1000;
-      }
-    }, 1000);
+    this.initTimer();
   }
 
   ngOnDestroy(): void {
     this.subscription?.unsubscribe();
-    clearInterval(this.timerInterval);
+    this.timerInterval && clearInterval(this.timerInterval);
   }
 
   onSkipQuestion() {
     this._store.dispatch(skipQuestion());
     // this._store.dispatch(startLoadingCurrentMovie());
+  }
+
+  private initTimer(): void {
+    this.timerInterval = setInterval(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime -= 1000;
+      } else {
+        console.log('clear');
+        clearInterval(this.timerInterval);
+      }
+    }, 1000);
   }
 }
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,2 +1,3 @@
-export const TIMER = 120000;
+const SECONDS = 1000;
+export const TIMER = 25 * SECONDS;
 export const CARDS_COUNT = 5;
